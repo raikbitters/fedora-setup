9b201b5cccce5dc77f1b27aa11ba30d0c88a4538
Raik Bitters <raikbitters@gmail.com>
2020-09-11 10:39:40 +0300

    Delete work apps

--- src/common/apps.py
+++ src/common/apps.py
@@ -3,12 +3,9 @@
 import os
 
 # Flathub apps (Telegram, Bitwarden, VLC, Remmina, Spotify)
-os.system("flatpak install -y flathub org.telegram.desktop com.bitwarden.desktop org.videolan.VLC org.remmina.Remmina com.spotify.Client")
+os.system("flatpak install -y flathub org.telegram.desktop com.bitwarden.desktop com.spotify.Client")
 
 # Chrome
 os.system("sudo dnf config-manager --set-enabled google-chrome")
 os.system("sudo dnf update -y")
 os.system("sudo dnf install -y google-chrome-stable")
-
-# Zoom
-os.system("sudo dnf install -y https://zoom.us/client/latest/zoom_x86_64.rpm")
