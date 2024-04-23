22ecb48c53222efa091a9d4c720a0765a4440636
Reingold Shekhtel <raikbitters@gmail.com>
2024-04-23 17:07:28 +0200

    Update fontawesome-fonts installation in fonts.py

--- src/fonts.py
+++ src/fonts.py
@@ -13,5 +13,5 @@ os.system("sudo dnf install fira-code-fonts")
 
 # Fonts for Oh My Zsh
 os.system('echo -e "\nFonts for Oh My Zsh"')
-os.system("sudo dnf install fontawesome-fonts")
+os.system("sudo dnf install fontawesome-fonts-all")
 os.system("pip install --user powerline-status")
