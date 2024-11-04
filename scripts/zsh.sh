e9c8a06f7af656114ab2da1971b3aad54c66504a
Reingold Shekhtel <raikbitters@gmail.com>
2024-11-04 13:51:26 +0100

    Add zsh config and theme

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -15,3 +15,9 @@ echo -e "\nInstall OhMyZsh plugins: zsh-syntax-highlighting zsh-autosuggestions 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
+
+# OhMyZsh theme
+cp -f ./assets/zsh/neo-robbyrussell.zsh-theme $ZSH_CUSTOM/themes/
+
+# Copy zsh configuration
+cp -f ./assets/zsh/.zshrc $HOME/.zshrc
