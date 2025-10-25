6d718ee7fdb2ec4ce2734532513281b51a209af7
raikbitters <raikbitters@gmail.com>
2025-10-25 14:24:30 +0200

    Fix rust.sh script by removing PATH export and sourcing from .bashrc

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -3,7 +3,3 @@
 # Install Rust using rustup
 echo -e "\nInstalling Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-# Add Rust to the PATH
-echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
